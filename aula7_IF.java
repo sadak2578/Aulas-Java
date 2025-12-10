45f81bddde7581759bb2c8049de494ca43e7d15f
sadak2578 <108498678+sadak2578@users.noreply.github.com>
2025-12-10 10:53:38 -0300

    aula7_IF.java

---
+++ aula7_IF.java
@@ -0,0 +1,15 @@
+package if_2;
+public class aula7_IF {
+    public static void main(String[] args) {
+      //o operador de exclamação inverte uma operação booleana
+      Boolean temCarteira = false;
+      
+      if (!temCarteira){
+        System.out.println("Você está autorizado para dirigir");
+      }else{
+        System.out.println("Você não tem autorização para dirigir");
+      }
+      
+      
+  }
+}
